9b2a5bbd03e1b368544797e17cd5d34dda4d7431
Lincon_programmer <lincon@gadbi.com.br>
2020-03-31 13:52:32 -0300

    adcionando procfile

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const app = require("./config/config");
-require("./config/mongo")('mongodb://localhost/coronadata');
+require("./config/mongo")('mongodb+srv://lincon:px29Lincon@cluster0-qionx.mongodb.net/test?retryWrites=true&w=majority');
 
-let porta = 3000;
+let porta = process.env.PORT || 3000;
 
 app.listen(porta, function(){
     console.log(`corona-data listening at port: ${porta}`);
